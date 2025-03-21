2bb1d3ce20ee5a3ac018fb700f7277bdedab34f5
parth <85664575+parthmodhvadiya@users.noreply.github.com>
2025-03-21 23:04:50 +0530

    Delete e-commerce-backend directory

--- e-commerce-backend/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('.')
-const {connectDB} = require('./config/db')
-
-const PORT = 5454;
-app.listen(PORT,async ()=>
-{
-    await connectDB();
-    console.log('E-commerce backend running on port: ',PORT);
-})
